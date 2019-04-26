f59c06626653262c66cbc1c78279aaa555c26637
nils <kula.nils@gmail.com>
2019-04-26 19:15:07 +0300

    finished

---
+++ src/main/java/io/codelex/accessingdatarest/AccessingDataRestApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.accessingdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataRestApplication.class, args);
+    }
+
+}
